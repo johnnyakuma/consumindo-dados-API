d1bbc6a3b6411e727b1abd1e5a6f9c468ece202d
Johnny Barbosa <66741283+johnnyakuma@users.noreply.github.com>
2023-09-25 12:28:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP Invalido. tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", ()=> buscaEndereco(cep.value));
+
+
+
+
+
+/*  //--- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+let ceps = ['01001000','01001001'];
+let conjuntosCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntosCeps).then(respostas => console.log(respostas));
+*/
